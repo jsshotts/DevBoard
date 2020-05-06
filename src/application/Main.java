c05345dff2aaf0d0110d1cf9143e6651107ddbed
ConnorBoulais <connorboulais@gmail.com>
2020-05-06 10:50:53 -0700

    Add stage title

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,8 +13,11 @@ public class Main extends Application {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("DevBoard");
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
