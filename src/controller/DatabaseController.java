1be2a762afc4338a3b6f6725207b2e7023de7d75
ConnorBoulais <connorboulais@gmail.com>
2020-05-18 13:08:16 -0700

    Convert to Maven Project and add Gson Dependency

--- src/controller/DatabaseController.java
+++ src/controller/DatabaseController.java
@@ -7,9 +7,16 @@ import entity.Project;
 import entity.ProjectOwner;
 import entity.Repository;
 
+import com.google.gson.Gson;
+
+
 public class DatabaseController {
 	
 	private Repository repo = new Repository();
+	
+	public DatabaseController() {
+		test();
+	}
 	
 	public List<Project> getAllProjects(){
 		return repo.projects;
@@ -21,5 +28,12 @@ public class DatabaseController {
 	
 	public List<ProjectOwner> getAllProjectOwners(){
 		return repo.projectOwners;
+	}
+	
+	
+	public void test() { 
+		Gson gson = new Gson();
+		//gson.toJson(new Project())
+;		System.out.println("in test");
 	}
 }
