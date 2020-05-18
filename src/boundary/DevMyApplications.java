0e3066cdea8155ceba80e3febeed7f4dc4f929d2
Jason Shotts <jsshotts@gmail.com>
2020-05-18 09:16:23 -0700

    cards show up in DevMyApplications.fxml

--- src/boundary/DevMyApplications.java
+++ src/boundary/DevMyApplications.java
@@ -1,12 +1,71 @@
 package boundary;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 public class DevMyApplications {
+	
+	@FXML
+	private ScrollPane projectOffers;
+	@FXML
+	private ScrollPane submittedApplications;
+	VBox projectOffersBox;
+	VBox submittedApplicationsBox;
+	
+	@FXML
+	private void initialize() {
+		
+		Task<Void> task = new Task<Void>() {
+			@Override
+			protected Void call() throws Exception {
+				projectOffersBox = initScrollView(WindowManager.OFFER_VIEW);
+				submittedApplicationsBox = initScrollView(WindowManager.SMALL_PROJECT_VIEW);
+				return null;
+			}
+		};
+		task.setOnSucceeded(succeededEvent -> {
+			projectOffers.setContent(projectOffersBox);
+			projectOffersBox.prefHeightProperty().bind(projectOffers.widthProperty());
+			projectOffersBox.prefWidthProperty().bind(projectOffers.widthProperty());
+			projectOffers.setFitToHeight(true);
+			
+			submittedApplications.setContent(submittedApplicationsBox);
+			submittedApplicationsBox.prefHeightProperty().bind(submittedApplications.widthProperty());
+			submittedApplicationsBox.prefWidthProperty().bind(submittedApplications.widthProperty());
+			submittedApplications.setFitToHeight(true);
+         });
+		ExecutorService executorService = Executors.newFixedThreadPool(1);
+		executorService.execute(task);
+        executorService.shutdown();
+	}
+	
+	private VBox initScrollView(String resource) {
+		VBox vbox = new VBox();
+		try {
+			
+			for(int i = 0; i < 10; i++) {
+				FXMLLoader fxmlLoader = new FXMLLoader(ClassLoader.getSystemResource(resource));
+				Node projectCard = fxmlLoader.load();
+				vbox.getChildren().add(projectCard);
+			}
+			System.out.println(vbox.getChildren());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return vbox;
+	}
+	
 	static void swapTo(ActionEvent event)
 	{
 		try {
